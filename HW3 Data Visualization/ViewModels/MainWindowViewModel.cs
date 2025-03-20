b8617786a533778d39a8985771a467df7577c5f6
Jonathan Kleffel <jklef24@student.sdu.dk>
2025-03-20 18:59:53 +0100

    Created HW3 Folder

---
+++ HW3 Data Visualization/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace HW3_Data_Visualization.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ HW3 Data Visualization/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace HW3_Data_Visualization.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
